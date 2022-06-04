6b2953e049a340c830ef0a8e2b7a70595824f88c
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-06-04 15:23:08 +0100

    drop year heatmap, add covalent_radius + first_ionization instead
    add physical units to multiselect options

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,13 +9,14 @@
   let windowWidth: number
 
   const heatmap_options: Record<string, keyof Element> = {
-    'Atomic Mass': `atomic_mass`,
-    'Atomic Radius': `atomic_radius`,
+    'Atomic Mass (u)': `atomic_mass`,
+    'Atomic Radius (Å)': `atomic_radius`,
+    'Covalent Radius (Å)': `covalent_radius`,
     Electronegativity: `electronegativity`,
-    Density: `density`,
-    'Boiling Point': `boiling_point`,
-    'Melting Point': `melting_point`,
-    'Year of Discovery': `year`,
+    'Density (solid: g/cm³, gas: g/liter)': `density`,
+    'Boiling Point (K)': `boiling_point`,
+    'Melting Point (K)': `melting_point`,
+    'First Ionization Energy (eV)': `first_ionization`,
   }
   $: heatmap_name = heatmap_options[selected_heatmap[0]] ?? null
 </script>
@@ -34,11 +35,13 @@
     placeholder="Select a heat map"
   />
 
-  <Table showNames={windowWidth > 1000} {heatmap_name} />
+  <Table show_names={windowWidth > 1000} {heatmap_name} />
 </main>
 
 <footer>
-  <a href="https://github.com/janosh/periodic-table">MIT License 2022</a>
+  <a href="https://github.com/janosh/periodic-table/blob/main/license">
+    MIT License 2022
+  </a>
 </footer>
 
 <style>
@@ -49,10 +52,16 @@
     --ghc-color: var(--page-bg);
     --ghc-bg: white;
     --sms-options-bg: black;
-    --sms-max-width: 16em;
+    --sms-max-width: 22em;
+    --sms-border: 1px dotted teal;
+    --sms-focus-border: 1px dotted cornflowerblue;
   }
   :global(div.multiselect) {
     margin: auto;
+  }
+  :global(div.multiselect input::placeholder) {
+    opacity: 0.8;
+    padding: 1ex;
   }
   h1 {
     text-align: center;
@@ -60,7 +69,7 @@
     font-size: clamp(20pt, 5.5vw, 50pt);
   }
   footer {
-    margin: 4em 0;
+    margin: 6em 0 0;
     text-align: center;
   }
 </style>
