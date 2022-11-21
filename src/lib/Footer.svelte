e9ea5952f20e01785bd34b4f699e5c6b7a9a906d
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-11-20 20:41:17 -0800

    remove netlify

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -24,7 +24,7 @@
     Bohr Atoms
   </a>
   <p class="toggle-icons">
-    <Toggle bind:checked={$show_icons} style="transform: scale(0.8);" />Show icons?
+    <Toggle bind:checked={$show_icons} style="transform: scale(0.8);" />Icons?
   </p>
 </footer>
 
--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@ import adapter from '@sveltejs/adapter-static'
 import { mdsvex } from 'mdsvex'
 import preprocess from 'svelte-preprocess'
 
+/** @type {import('@sveltejs/kit').Config} */
 export default {
   extensions: [`.svelte`, `.svx`, `.md`],
 
