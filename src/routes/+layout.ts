7b17a5adb013f46ce5141fdb59fdc90c8bbdcdae
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-01-01 15:21:14 -0800

    v0.1.0
    add changelog

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,10 @@
+import { redirect } from '@sveltejs/kit'
+import type { LayoutLoad } from './$types'
+
 export const prerender = true
+
+export const load: LayoutLoad = ({ url }) => {
+  if (url.pathname.endsWith(`.md`)) {
+    throw redirect(307, url.pathname.replace(/\.md$/, ``))
+  }
+}
---
+++ src/routes/changelog/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Toc from 'svelte-toc'
+  import Changelog from '../../../changelog.md'
+</script>
+
+<main>
+  <Changelog />
+</main>
+
+<Toc headingSelector="main > :where(h3, h4)" breakpoint={1250} />
