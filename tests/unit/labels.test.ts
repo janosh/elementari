3863a6cdd802a404f9d599c7fae18e7d95dc1384
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-04-03 11:26:11 -0700

    refactor pretty_num() to use d3.format() with SI units for abs(num) > 1 and floating or exponential for abs(num) < 1

--- tests/unit/labels.test.ts
+++ tests/unit/labels.test.ts
@@ -1,5 +1,10 @@
 import { element_data } from '$lib'
-import { heatmap_keys, pretty_num, property_labels } from '$lib/labels'
+import {
+  default_precision,
+  heatmap_keys,
+  pretty_num,
+  property_labels,
+} from '$lib/labels'
 import { expect, test } from 'vitest'
 
 test(`pretty_num`, () => {
@@ -7,20 +12,35 @@ test(`pretty_num`, () => {
   expect(pretty_num(1)).toBe(`1`)
   expect(pretty_num(10)).toBe(`10`)
   expect(pretty_num(100)).toBe(`100`)
-  expect(pretty_num(1_000)).toBe(`1,000`)
-  expect(pretty_num(10_000)).toBe(`10,000`)
-  expect(pretty_num(100_000)).toBe(`100,000`)
-  expect(pretty_num(1_000_000)).toBe(`1,000,000`)
-  expect(pretty_num(10_000_000)).toBe(`1.00e+7`)
-  expect(pretty_num(100_000_000)).toBe(`1.00e+8`)
-  expect(pretty_num(1_000_000_000)).toBe(`1.00e+9`)
+  expect(pretty_num(1_000)).toBe(`1k`)
+  expect(pretty_num(10_000)).toBe(`10k`)
+  expect(pretty_num(100_000)).toBe(`100k`)
+  expect(pretty_num(1_000_000)).toBe(`1M`)
+  expect(pretty_num(10_000_000)).toBe(`10M`)
+  expect(pretty_num(100_000_000)).toBe(`100M`)
+  expect(pretty_num(1_000_000_000)).toBe(`1G`)
 
   expect(pretty_num(0.1)).toBe(`0.1`)
   expect(pretty_num(0.01)).toBe(`0.01`)
-  expect(pretty_num(0.001)).toBe(`1.00e-3`)
-  expect(pretty_num(-0.0001)).toBe(`-1.00e-4`)
+  expect(pretty_num(0.001)).toBe(`0.001`)
+  // TODO: figure out how to make pretty_num(-0.0001) = '1e-4'
+  expect(pretty_num(-0.000_1)).toBe(`−0.0001`) // want −1e-4
+  expect(pretty_num(-0.000_01)).toBe(`−0.00001`) // want −1e-5
+  expect(pretty_num(-0.000_001)).toBe(`−0.000001`) // want −1e-6
+  expect(pretty_num(-0.000_000_1)).toBe(`−1e-7`)
 
-  expect(pretty_num(-1.1)).toBe(`-1.1`)
+  expect(pretty_num(-1.1)).toBe(`−1.1`)
+  expect(pretty_num(-1.14123)).toBe(`−1.14`)
+  expect(pretty_num(-1.14123e-7, `.5~g`)).toBe(`−1.1412e-7`)
+})
+
+test(`default_precision`, () => {
+  expect(default_precision).toEqual([`,.3~s`, `.3~g`])
+
+  expect(pretty_num(12_345)).toBe(`12.3k`)
+  default_precision[0] = `,.5~s`
+  expect(pretty_num(12_345)).toBe(`12.345k`)
+  default_precision[0] = `,.3~s`
 })
 
 const element_data_keys = Object.keys(element_data[0])
