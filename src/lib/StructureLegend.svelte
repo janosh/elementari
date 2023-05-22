7a3558a301a9bb2c09afb0cb7ae1a9b2f4a134e6
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-05-21 21:33:32 -0700

    add lib/StructureLegend.svelte with new color scheme <select> and <input type='color'> for overriding individual element colors

---
+++ src/lib/StructureLegend.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+  import { Tooltip } from 'svelte-zoo'
+  import type { ElementSymbol } from '.'
+  import { element_data } from '.'
+  import { default_element_colors } from './colors'
+  import { get_text_color } from './labels'
+  import { element_colors } from './stores'
+
+  export let elements: ElementSymbol[] = []
+  export let elem_color_picker_title: string = 'Double click to reset color'
+  export let labels: HTMLLabelElement[] = []
+  export let style: string | null = null
+</script>
+
+<div {style}>
+  {#each elements as elem, idx}
+    <Tooltip
+      text={element_data.find((el) => el.symbol == elem)?.name}
+      --zoo-tooltip-bg="rgba(255, 255, 255, 0.3)"
+    >
+      <label
+        bind:this={labels[idx]}
+        style="background-color: {$element_colors[elem]}"
+        on:dblclick|preventDefault={() =>
+          ($element_colors[elem] = default_element_colors[elem])}
+        style:color={get_text_color(labels[idx])}
+      >
+        {elem}
+        <input
+          type="color"
+          bind:value={$element_colors[elem]}
+          title={elem_color_picker_title}
+        />
+      </label>
+    </Tooltip>
+  {/each}
+</div>
+
+<style>
+  div {
+    display: flex;
+    position: absolute;
+    bottom: var(--struct-elem-list-bottom, 8pt);
+    right: var(--struct-elem-list-right, 8pt);
+    gap: var(--struct-elem-list-gap, 8pt);
+    font-size: var(--struct-elem-list-font-size, 16pt);
+  }
+  div label {
+    padding: 1pt 4pt;
+    border-radius: var(--struct-elem-list-border-radius, 3pt);
+  }
+  div label input[type='color'] {
+    z-index: 1;
+    opacity: 0;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    cursor: pointer;
+  }
+</style>
