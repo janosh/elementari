ccb1396d828dfbdad7cd7048b183de31937f0abc
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-06-29 19:19:07 -0700

    add lib/structure/Bond.svelte

---
+++ src/lib/structure/Bond.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { Instance } from '@threlte/extras'
+  import { Euler, Quaternion, Vector3 } from 'three'
+
+  export let from: [number, number, number]
+  export let to: [number, number, number]
+  export let offset: number = 0
+  export let radius: number = 0.1
+  export let color: string = `white`
+
+  const from_vec = new Vector3(...from)
+  const to_vec = new Vector3(...to)
+
+  const { position, rotation, height } = calc_bond(from_vec, to_vec, offset, radius)
+
+  function calc_bond(from_vec: Vector3, to_vec: Vector3, offset: number, radius: number) {
+    // find the axis of the the box
+    const delta_vec = to_vec.clone().sub(from_vec)
+    // length of the bond
+    const height = delta_vec.length()
+    // calculate position
+    let position: [number, number, number]
+    if (offset === 0) {
+      position = from_vec.clone().add(delta_vec.multiplyScalar(0.5)).toArray()
+    } else {
+      const offset_vec = new Vector3()
+        .crossVectors(delta_vec, new Vector3(1, 0, 0))
+        .normalize()
+      position = from_vec
+        .clone()
+        .add(delta_vec.multiplyScalar(0.5))
+        .add(offset_vec.multiplyScalar(offset * radius * 2))
+        .toArray()
+    }
+    // calculate rotation
+    const quaternion = new Quaternion().setFromUnitVectors(
+      new Vector3(0, 1, 0),
+      delta_vec.normalize()
+    )
+    const rotation = new Euler().setFromQuaternion(quaternion).toArray()
+    // return results
+    return { height, position, rotation }
+  }
+</script>
+
+<Instance {position} {rotation} scale={height} {color} />
