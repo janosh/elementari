e0dbc5533feb3095c0d3dfeb7d364a85ce991191
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-07-08 14:48:17 -0700

    sort JSON structures by MP ID number

--- src/site/index.ts
+++ src/site/index.ts
@@ -8,8 +8,15 @@ export const structures = Object.entries(
     eager: true,
     import: `default`,
   }) as Record<string, IdStructure>,
-).map(([path, structure]) => {
-  const id = path.split(`/`).at(-1)?.split(`.`)[0] as string
-  structure.id = id
-  return structure
-})
+)
+  .map(([path, structure]) => {
+    const id = path.split(`/`).at(-1)?.split(`.`)[0] as string
+    structure.id = id
+    return structure
+  })
+  .sort((struct1, struct2) => {
+    const [n1, n2] = [struct1, struct2].map((struct) =>
+      struct.id.split(`-`)[1].padStart(6, `0`),
+    )
+    return n1.localeCompare(n2)
+  })
